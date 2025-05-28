c3012e3f7e834554f077acaed97534a472fd3a8a
Jfouyf <ysun805@students.fairmontschools.com>
2025-05-28 11:02:47 -0700

    new

--- Pathfinding.java
+++ Pathfinding.java
@@ -2,64 +2,69 @@ import java.util.*;
 
 public class Pathfinding {
    
-    static int[][] A = {
-        {1,0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,0,0,0,0,1,0,0},
-        {9,0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,2,0,0,0,0,0,0},
-        {1,0,0,1,1,1,1,1,1,1,1}
-    };
-
-    public static void main(String[] args) {
-       
-        printPathAndGraph();
-    }
+    static ArrayList<ArrayList<Integer>> A = new ArrayList<>(Arrays.asList(
+        new ArrayList<>(Arrays.asList(0, 0, 1, 0, 0, 0, 0, 0)),
+        new ArrayList<>(Arrays.asList(0, 0, 1, 0, 0, 0, 0, 0)),
+        new ArrayList<>(Arrays.asList(0, 0, 1, 0, 0, 0, 1, 0)),
+        new ArrayList<>(Arrays.asList(1, 1, 1, 0, 0, 0, 0, 0)),
+        new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0)),
+        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)),
+        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 2, 0, 0, 0)),
+        new ArrayList<>(Arrays.asList(1, 0, 0, 0, 1, 1, 1, 1))
+    ));
 
     private static void printPathAndGraph() {
-       
+
         List<Pos> path = findPathPositions();
 
-        
-        List<String> formatted = new ArrayList<>();
-        for (Pos p : path) {
-            formatted.add(format(p));
-        }
-        System.out.println("Graph:");
-        System.out.println(formatted);
 
-        
-        int n = A.length, m = A[0].length;
-        int[][] graph = new int[n][m];
+        List<String> coords = new ArrayList<>();
         for (Pos p : path) {
-            graph[p.r][p.c] = 1;
+            coords.add("A[" + p.r + "][" + p.c + "]");
         }
+        System.out.println("graph：");
+        System.out.println(coords);
 
-        
-        System.out.println("\nonly contains 1");
+
+        int n = A.size(), m = A.get(0).size();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-           
-            System.out.println(Arrays.toString(graph[i]));
+            graph.add(new ArrayList<>(Collections.nCopies(m, 0)));
+        }
+
+        for (Pos p : path) {
+            graph.get(p.r).set(p.c, 1);
+        }
+
+        System.out.println("\nonly contains 1：");
+        for (ArrayList<Integer> row : graph) {
+            System.out.println(row);
         }
     }
 
- 
+
     private static List<Pos> findPathPositions() {
-        int n = A.length, m = A[0].length;
+        int n = A.size(), m = A.get(0).size();
         List<Pos> ends = new ArrayList<>();
+
+ 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) if (A[i][j] == 1) {
-                int cnt = 0;
-                for (int[] d : DIR) {
-                    int ni = i + d[0], nj = j + d[1];
-                    if (ni>=0 && ni<n && nj>=0 && nj<m && A[ni][nj]==1) cnt++;
+            for (int j = 0; j < m; j++) {
+                if (A.get(i).get(j) == 1) {
+                    int cnt = 0;
+                    for (int[] d : DIR) {
+                        int ni = i + d[0], nj = j + d[1];
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < m && A.get(ni).get(nj) == 1) {
+                            cnt++;
+                        }
+                    }
+                    if (cnt == 1) ends.add(new Pos(i, j));
                 }
-                if (cnt == 1) ends.add(new Pos(i,j));
             }
         }
 
-        
+
         List<Pos> best = new ArrayList<>();
         for (Pos start : ends) {
             Pos prev = null, cur = start;
@@ -69,9 +74,9 @@ public class Pathfinding {
                 Pos next = null;
                 for (int[] d : DIR) {
                     int ni = cur.r + d[0], nj = cur.c + d[1];
-                    if (ni>=0 && ni<n && nj>=0 && nj<m && A[ni][nj]==1) {
-                        if (prev == null || !(prev.r==ni && prev.c==nj)) {
-                            next = new Pos(ni,nj);
+                    if (ni >= 0 && ni < n && nj >= 0 && nj < m && A.get(ni).get(nj) == 1) {
+                        if (prev == null || !(prev.r == ni && prev.c == nj)) {
+                            next = new Pos(ni, nj);
                             break;
                         }
                     }
@@ -79,22 +84,24 @@ public class Pathfinding {
                 prev = cur;
                 cur = next;
             }
-            if (path.size() > best.size()) best = path;
+            if (path.size() > best.size()) {
+                best = path;
+            }
         }
         return best;
     }
 
+
     private static final int[][] DIR = {
-        {-1,0}, {0,1}, {1,0}, {0,-1}
+        {-1, 0}, {0, 1}, {1, 0}, {0, -1}
     };
+
 
     private static class Pos {
         int r, c;
         Pos(int r, int c) { this.r = r; this.c = c; }
     }
-
-    private static String format(Pos p) {
-        return "A[" + p.r + "][" + p.c + "]";
+    public static void main(String[] args) {
+        printPathAndGraph();
     }
 }
-
