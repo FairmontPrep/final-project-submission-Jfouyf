63350581b86d29fa2865f97f442148983acc0bcf
Jfouyf <ysun805@students.fairmontschools.com>
2025-05-28 10:15:01 -0700

    change

--- PathfindingRobot.java
+++ PathfindingRobot.java
@@ -2,75 +2,76 @@ import java.util.*;
 
 public class PathfindingRobot {
 
-
-    static int[][] A = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-    };
+    static ArrayList<ArrayList<Integer>> map;
+    static int startRow = -1, startCol = -1;
 
     public static void main(String[] args) {
         runPathfinding();
     }
-    
+
     public static void runPathfinding() {
-        ArrayList<String> answerList = findPath(A);
-        System.out.println(answerList);
-        printPath(answerList, A.length, A[0].length);
-    }
-    
+        // Sample input - replace with any test array from Client
+        map = new ArrayList<>();
+        map.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 0, 0, 0, 0)));
+        map.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)));
+        map.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 1, 0)));
+        map.add(new ArrayList<>(Arrays.asList(9, 0, 0, 1, 0, 0, 0, 0)));
+        map.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)));
+        map.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)));
+        map.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 2, 0, 0, 0)));
+        map.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 1, 1, 1, 1)));
 
-    public static ArrayList<String> findPath(int[][] map) {
+        int rows = map.size(), cols = map.get(0).size();
+        int[][] grid = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                grid[i][j] = map.get(i).get(j) == 1 ? 1 : 0;
+
+        locateStartingPoint(grid);
+
         ArrayList<String> path = new ArrayList<>();
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    if (dfs(map, i, j, path, visited)) {
-                        return path;
-                    } else {
-                        path.clear();
-                    }
-                }
-            }
+        boolean[][] visited = new boolean[rows][cols];
+
+        if (startRow != -1 && dfs(grid, startRow, startCol, path, visited)) {
+            System.out.println(path);
+            printPath(path, rows, cols);
+        } else {
+            System.out.println("No valid path found.");
         }
-        return path;
     }
 
+    public static void locateStartingPoint(int[][] map) {
+        int rows = map.length, cols = map[0].length;
+        for (int i = 0; i < rows; i++) {
+            if (map[i][0] == 1) { startRow = i; startCol = 0; return; }
+            if (map[i][cols - 1] == 1) { startRow = i; startCol = cols - 1; return; }
+        }
+        for (int j = 0; j < cols; j++) {
+            if (map[0][j] == 1) { startRow = 0; startCol = j; return; }
+            if (map[rows - 1][j] == 1) { startRow = rows - 1; startCol = j; return; }
+        }
+    }
 
     public static boolean dfs(int[][] map, int r, int c, ArrayList<String> path, boolean[][] visited) {
-        if (r < 0 || c < 0 || r >= map.length || c >= map[0].length || map[r][c] != 1 || visited[r][c]) {
+        if (r < 0 || c < 0 || r >= map.length || c >= map[0].length || map[r][c] != 1 || visited[r][c])
             return false;
-        }
+
         visited[r][c] = true;
         path.add("A[" + r + "][" + c + "]");
 
-        if (isEndOfPath(map, r, c, visited)) return true;
+        if ((r != startRow || c != startCol) && isWall(r, c, map)) return true;
 
-        if (dfs(map, r + 1, c, path, visited)) return true;
-        if (dfs(map, r - 1, c, path, visited)) return true;
-        if (dfs(map, r, c + 1, path, visited)) return true;
-        if (dfs(map, r, c - 1, path, visited)) return true;
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        for (int[] d : dirs)
+            if (dfs(map, r + d[0], c + d[1], path, visited)) return true;
 
-        // backtrack
         path.remove(path.size() - 1);
         return false;
     }
 
-    public static boolean isEndOfPath(int[][] map, int r, int c, boolean[][] visited) {
-        int count = 0;
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        for (int[] d : dirs) {
-            int nr = r + d[0], nc = c + d[1];
-            if (nr >= 0 && nc >= 0 && nr < map.length && nc < map[0].length) {
-                if (map[nr][nc] == 1 && !visited[nr][nc]) count++;
-            }
-        }
-        return count == 0;
+    public static boolean isWall(int r, int c, int[][] map) {
+        return r == 0 || r == map.length - 1 || c == 0 || c == map[0].length - 1;
     }
 
     public static void printPath(ArrayList<String> path, int rows, int cols) {
